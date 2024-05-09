064e58eeae83ab1bbd7f3874481137ea20ca2ede
miguelitodev <miguelrisquelme@gmail.com>
2024-05-09 18:01:35 -0300

    feat: Adding responsive part

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,23 +5,23 @@ export default function Home() {
 	return (
 		<div className="flex justify-center flex-col items-center h-screen bg-gradient-to-br from-opacity-10 to-transparent backdrop-blur-2xl shadow-lg p-6">
 			<div className="w-auto text-white font-bold">
-				<span className="text-3xl font-mono">Hi, my name is</span>
-				<h1 className="text-8xl font-bold leading-normal font-mono tracking-wider text-center bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient">
+				<span className="font-mono text-sm md:text-md lg:text-xl">
+					Hi, my name is
+				</span>
+				<h1 className="text-3xl md:text-6xl lg:text-8xl font-bold leading-loose lg:leading-normal font-mono tracking-wider text-center bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient">
 					Miguel Riquelme
 				</h1>
 
 				<div className="text-right">
-					<span className="font-mono">
+					<span className="font-mono text-sm">
 						and I am <br />
 						<TypeAnimation
 							sequence={[
-								// Same substring at the start will only be typed out once, initially
 								"Mid-level developer",
-								1000, // wait 1s before replacing "Mice" with "Hamsters"
+								1000,
 								"",
-								// Same substring at the start will only be typed out once, initially
 								"Front-end developer",
-								1000, // wait 1s before replacing "Mice" with "Hamsters"
+								1000,
 								"",
 								1000,
 								"Designer",
