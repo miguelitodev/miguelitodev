8160eefb35fd35220d33f0031b9edfa48b83b28d
root <root@DESKTOP-TE3TRNR>
2024-05-20 18:43:35 -0300

    feat: Adding my personal summary

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
-import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
+import { format } from "date-fns";
+import axios from "axios";
+import Link from "next/link";
+import { TypeAnimation } from "react-type-animation";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/solid";
+
 import { ButtonFlashing } from "@/components";
 import { ArrowRainbowDown } from "@/assets/icons";
 import { socialMedias } from "@/data";
-import Link from "next/link";
-import { format } from "date-fns";
-import axios from "axios";
-import { stringify } from "querystring";
 
 type GeolocationCoordinates = {
 	latitude: number;
@@ -27,7 +28,14 @@ type GeolocationPosition = {
 export default function Home() {
 	const [opened, setOpened] = useState<boolean>(false);
 	const [contactOpen, setContactOpen] = useState<boolean>(false);
-	const [location, setLocation] = useState({ tz_id: "", text: "" });
+	const [location, setLocation] = useState({
+		tz_id: "",
+		text: "",
+		country: "",
+	});
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleOpen = () => setIsOpen(!isOpen);
 
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition(
@@ -40,6 +48,7 @@ export default function Home() {
 						console.log(data);
 
 						setLocation({
+							country: data.location.country,
 							tz_id: data.tz_id,
 							text: `${data.location.name}, ${data.location.region} - ${data.current.temp_c}°C / ${data.current.temp_f}°F`,
 						});
@@ -127,12 +136,12 @@ export default function Home() {
 	if (!opened) return <Welcome />;
 
 	return (
-		<div className="h-screen bg-gradient-to-br from-opacity-10 to-transparent backdrop-blur-2xl shadow-lg p-6">
+		<div className="h-screen bg-gradient-to-br from-opacity-10 to-transparent backdrop-blur-2xl shadow-lg p-6 flex items-center justify-between flex-col">
 			<motion.div
 				initial={{ top: 300, opacity: 0 }}
 				animate={{ top: 0, opacity: 1 }}
 				exit={{ top: -300, opacity: 0 }}
-				className="relative flex flex-col items-center"
+				className="w-full relative flex flex-col items-center"
 			>
 				<div className="xl:w-1/2 w-full p-4 m-auto">
 					<header className="w-full flex items-center justify-between">
@@ -237,6 +246,68 @@ export default function Home() {
 					</motion.div>
 				)}
 			</motion.div>
+
+			<div className="w-1/2 flex flex-col gap-4">
+				<h1 className="text-white font-bold text-5xl">Hello</h1>
+				<p className="text-white font-extralight">
+					Welcome to my world. I would like to present to you my inner self, all
+					the achievements I have been working on, and the person I have become.
+					I hope you appreciate it.
+				</p>
+
+				<motion.div
+					initial={{ height: 0, opacity: 0 }}
+					animate={{ height: isOpen ? "auto" : 0, opacity: isOpen ? 1 : 0 }}
+					transition={{ duration: 0.3 }}
+					className="overflow-hidden flex flex-col gap-4"
+				>
+					<p className="text-white font-extralight">
+						A summary of myself: I was born in 2002, and since high school, I
+						have been working with frontend development. I have a degree in
+						Software Analysis and Development and have been working for 5 years
+						in the same field, so I consider myself a Senior Frontend Developer,
+						specializing in the Javascript ecosystem. I have worked with
+						React.js and React Native, and now I am improving my knowledge in
+						Next.js and Typescript. I enjoy learning new things and am excellent
+						at troubleshooting. I am known for the speed of my deliveries.
+					</p>
+					<p className="text-white font-extralight">
+						What I want for the future: to work abroad. My goal is to go to the
+						USA or countries in the European Union. My dream is to travel around
+						the world and live a life where I can grow without impediments. I am
+						improving my English and continuing to study technology so that one
+						day I can achieve this dream through my hard work.
+					</p>
+					<div className="flex justify-end item-center">
+						<a
+							href={`pdfs/${
+								location.country === "Brazil"
+									? "curriculum-ptBR.pdf"
+									: "curriculum-enUS.pdf"
+							}`}
+							download
+						>
+							<ButtonFlashing>Curriculum</ButtonFlashing>
+						</a>
+					</div>
+				</motion.div>
+				<motion.button
+					whileHover={{ scale: 1.1 }}
+					whileTap={{ scale: 0.9 }}
+					onClick={toggleOpen}
+					className="mx-auto mt-4 text-white"
+				>
+					{isOpen ? (
+						<ChevronUpIcon className="w-6 h-6" />
+					) : (
+						<ChevronDownIcon className="w-6 h-6" />
+					)}
+				</motion.button>
+			</div>
+
+			<div className="text-white font-thin text-xs">
+				Made with ❤ by Miguel Riquelme
+			</div>
 		</div>
 	);
 }
