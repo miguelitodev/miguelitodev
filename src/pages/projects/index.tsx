92702f8c9422f66cd4ae6fb58046a13fb271fda4
Miguel Riquelme <miguelrisquelme@gmail.com>
2025-02-10 16:57:00 -0300

    feat: fixing type error

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-floating-promises */
+
 import { Page } from "@/template";
 import { useEffect, useState } from "react";
 import axios from "axios"; // Importando axios
